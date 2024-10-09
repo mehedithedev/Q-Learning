0f906b6bae31c27c92fe912945504bf42d32c046
Aden John <Adenthedev@gmail.com>
2024-10-08 22:08:13 -0600

    Changed the reward and agent's image

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 import tkinter as tk
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageSequence
 
 np.random.seed(1)
 PhotoImage = ImageTk.PhotoImage
@@ -18,8 +18,11 @@ class Env(tk.Tk):
         self.title('Q Learning')
         self.geometry('{0}x{1}'.format(HEIGHT * UNIT, HEIGHT * UNIT))
         self.shapes = self.load_images()
+        self.fire_frames = self.load_fire_frames()  # Initialize fire_frames before _build_canvas
         self.canvas = self._build_canvas()
         self.texts = []
+        self.current_fire_frame = 0
+        self.animate_fire()
 
     def _build_canvas(self):
         canvas = tk.Canvas(self, bg='white',
@@ -28,17 +31,17 @@ class Env(tk.Tk):
         # create grids
         for c in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
             x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
-            canvas.create_line(x0, y0, x1, y1)
+            canvas.create_line(x0, y0, x1, y1, fill='light grey')
         for r in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
             x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
-            canvas.create_line(x0, y0, x1, y1)
+            canvas.create_line(x0, y0, x1, y1, fill='light grey')
 
         # add img to canvas
-        self.rectangle = canvas.create_image(50, 50, image=self.shapes[0])
-        self.triangle1 = canvas.create_image(250, 150, image=self.shapes[1])
-        self.triangle2 = canvas.create_image(150, 250, image=self.shapes[1])
-        self.triangle3 = canvas.create_image(250, 350, image=self.shapes[1])
-        self.circle = canvas.create_image(250, 250, image=self.shapes[2])
+        self.q = canvas.create_image(50, 50, image=self.shapes[0])
+        self.fire1 = canvas.create_image(250, 150, image=self.fire_frames[0])
+        self.fire2 = canvas.create_image(150, 250, image=self.fire_frames[0])
+        self.fire3 = canvas.create_image(250, 350, image=self.fire_frames[0])
+        self.todd = canvas.create_image(250, 250, image=self.shapes[2])
 
         # pack all
         canvas.pack()
@@ -46,14 +49,26 @@ class Env(tk.Tk):
         return canvas
 
     def load_images(self):
-        rectangle = PhotoImage(
+        rectangle = ImageTk.PhotoImage(
             Image.open("./img/rectangle.png").resize((65, 65)))
-        triangle = PhotoImage(
-            Image.open("./img/triangle.png").resize((65, 65)))
-        circle = PhotoImage(
-            Image.open("./img/circle.png").resize((65, 65)))
+        fire = ImageTk.PhotoImage(
+            Image.open("./img/fire.gif").resize((65, 65)))
+        todd = ImageTk.PhotoImage(
+            Image.open("./img/todd.png").resize((190, 100)))  # Adjust size as needed
 
-        return rectangle, triangle, circle
+        return [rectangle, fire, todd]
+
+    def load_fire_frames(self):
+        fire_image = Image.open("./img/fire.gif")
+        frames = [ImageTk.PhotoImage(frame.copy().resize((65, 65))) for frame in ImageSequence.Iterator(fire_image)]
+        return frames
+
+    def animate_fire(self):
+        self.current_fire_frame = (self.current_fire_frame + 1) % len(self.fire_frames)
+        self.canvas.itemconfig(self.fire1, image=self.fire_frames[self.current_fire_frame])
+        self.canvas.itemconfig(self.fire2, image=self.fire_frames[self.current_fire_frame])
+        self.canvas.itemconfig(self.fire3, image=self.fire_frames[self.current_fire_frame])
+        self.after(100, self.animate_fire)  # Adjust the delay as needed
 
     def text_value(self, row, col, contents, action, font='Helvetica', size=10,
                    style='normal', anchor="nw"):
@@ -63,53 +78,18 @@ class Env(tk.Tk):
         elif action == 1:
             origin_x, origin_y = 85, 42
         elif action == 2:
-            origin_x, origin_y = 42, 5
-        else:
-            origin_x, origin_y = 42, 77
+            origin_x, origin_y = 7, 82
+        elif action == 3:
+            origin_x, origin_y = 85, 82
 
-        x, y = origin_y + (UNIT * col), origin_x + (UNIT * row)
-        font = (font, str(size), style)
-        text = self.canvas.create_text(x, y, fill="black", text=contents,
-                                       font=font, anchor=anchor)
-        return self.texts.append(text)
-
-    def print_value_all(self, q_table):
-        for i in self.texts:
-            self.canvas.delete(i)
-        self.texts.clear()
-        for i in range(HEIGHT):
-            for j in range(WIDTH):
-                for action in range(0, 4):
-                    state = [i, j]
-                    if str(state) in q_table.keys():
-                        temp = q_table[str(state)][action]
-                        self.text_value(j, i, round(temp, 2), action)
-
-    def coords_to_state(self, coords):
-        x = int((coords[0] - 50) / 100)
-        y = int((coords[1] - 50) / 100)
-        return [x, y]
-
-    def state_to_coords(self, state):
-        x = int(state[0] * 100 + 50)
-        y = int(state[1] * 100 + 50)
-        return [x, y]
-
-    def reset(self):
-        self.update()
-        time.sleep(0.5)
-        x, y = self.canvas.coords(self.rectangle)
-        self.canvas.move(self.rectangle, UNIT / 2 - x, UNIT / 2 - y)
-        self.render()
-        # return observation
-        return self.coords_to_state(self.canvas.coords(self.rectangle))
-
+        self.texts.append(self.canvas.create_text(
+            origin_x + (UNIT * col), origin_y + (UNIT * row),
+            text=contents, font=(font, size, style), anchor=anchor))
 
     def step(self, action):
-        state = self.canvas.coords(self.rectangle)
-        base_action = np.array([0, 0])
-        self.render()
-
+        state = self.canvas.coords(self.q)
+        base_action = [0, 0]
+        
         if action == 0:  # up
             if state[1] > UNIT:
                 base_action[1] -= UNIT
@@ -123,28 +103,38 @@ class Env(tk.Tk):
             if state[0] < (WIDTH - 1) * UNIT:
                 base_action[0] += UNIT
 
-        # move agent
-        self.canvas.move(self.rectangle, base_action[0], base_action[1])
-        # move rectangle to top level of canvas
-        self.canvas.tag_raise(self.rectangle)
-        next_state = self.canvas.coords(self.rectangle)
+        # Move the agent
+        self.canvas.move(self.q, base_action[0], base_action[1])
+        self.update()
 
-        # reward function
-        if next_state == self.canvas.coords(self.circle):
-            reward = 100
+        # Next state
+        next_state = self.canvas.coords(self.q)
+
+        # Reward function
+        if next_state == self.canvas.coords(self.todd):
+            reward = 1
             done = True
-        elif next_state in [self.canvas.coords(self.triangle1),
-                            self.canvas.coords(self.triangle2),
-                             self.canvas.coords(self.triangle3)]:
-            reward = -100
+        elif next_state in [self.canvas.coords(self.fire1), self.canvas.coords(self.fire2), self.canvas.coords(self.fire3)]:
+            reward = -1
             done = True
         else:
             reward = 0
             done = False
 
-        next_state = self.coords_to_state(next_state)
         return next_state, reward, done
+
+    def reset(self):
+        self.update()
+        time.sleep(0.5)
+        self.canvas.delete(self.q)
+        self.q = self.canvas.create_image(50, 50, image=self.shapes[0])
+        return self.canvas.coords(self.q)
 
     def render(self):
         time.sleep(0.03)
         self.update()
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.mainloop()
